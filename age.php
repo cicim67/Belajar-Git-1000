32a5fb10c849917be26a5a2d377865cc597dfb2c
rizkyreza <rizky.fahmi@students.amikom.ac.id>
2016-12-09 09:46:42 +0700

    tambah apa

---
+++ age.php
@@ -0,0 +1,22 @@
+<?php
+// Nama  	:Doni Satrio Nama
+// NIM		:NIM
+
+class age{
+	public $umur;
+
+	function __construct($date){
+		$this->umur = $date;
+	}
+	
+	function getUmur(){
+		if(!empty($this->umur)){
+			
+		$interval = date_diff(date_create(), date_create($this->umur));
+		return $interval->format("%y Tahun, %m Bulan, %d Hari");
+		}else {
+			return "<font color='red'>data tidak falid</font>";
+		}
+	}
+	}
+?>
